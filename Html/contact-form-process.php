d3db6bc06574afe2fa5d452843c18b434bc07fc6
Shivam Basak <90944419+shivamBasak@users.noreply.github.com>
2022-09-07 18:40:55 +0530

    d

--- Html/contact-form-process.php
+++ Html/contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
+    $email_to = "shivambasak70@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
